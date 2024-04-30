2cacde8319327396c86796a06f6da81ef34f1237
LucMagg <luc.maggiotto@gmail.com>
2024-04-30 14:46:14 +0200

    fix js : modif errorPage
    ajout du document outline de la page d'erreur

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -9,6 +9,7 @@ export default function ErrorPage() {
 
     return (<Layout>
         <main className='error-main'>
+            <h2 className='error-main__title'>Erreur</h2>
             <p className='error-main__status'>404</p>
             <div className='error-main__message'>
                 <span className='error-main__message__block'>Oups! La page que </span>
